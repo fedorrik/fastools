b4b87e1a152d2a21fea3b2b0b03f494492d298c1
fedor <fedorrik1@gmail.com>
2021-06-19 11:59:10 +0300

    fix sort script

--- scripts/sort.py
+++ scripts/sort.py
@@ -2,7 +2,7 @@ from Bio.SeqIO import parse, write
 from sys import argv
 
 
-data = [[record.id, record.seq] for record in parse(argv[1], 'fasta')]
+data = [[record.description, record.seq] for record in parse(argv[1], 'fasta')]
 for i in sorted(data):
     print('>{}\n{}'.format(i[0], i[1]))
 
