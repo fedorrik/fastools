0b6987ea3f5a80e09012a27c681a271354f5b17a
Fedor <fedorrik1@gmail.com>
2022-03-11 15:47:27 +0300

    update split script

--- scripts/split.py
+++ scripts/split.py
@@ -1,6 +1,8 @@
 from sys import argv
 from Bio.SeqIO import parse, write
 
+
+out_dir = argv[2]
 for record in parse(argv[1], 'fasta'):
-    write(record, '{}.fa'.format(record.id), 'fasta')
+    write(record, '{}/{}.fa'.format(out_dir, record.id), 'fasta')
 
