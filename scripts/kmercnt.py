01b0181d309d5edac41637731817f42f19316fa9
fedorrik <fedorrik1@gmail.com>
2022-07-27 22:25:28 +0300

    upgrade kmercnt

--- scripts/kmercnt.py
+++ scripts/kmercnt.py
@@ -5,7 +5,12 @@ from sys import argv
 
 kmer = argv[2].upper()
 
+total_hits = 0
 for record in parse(argv[1], 'fasta'):
     n_hits = len(findall(kmer, str(record.seq).upper()))
-    print(record.id, n_hits)
+    print(record.id, n_hits, sep='\t')
+    total_hits += n_hits
+
+print()
+print(argv[1], total_hits, sep='\t')
 
