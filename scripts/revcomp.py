378aff37f02a76b9c4bd9e252e6b2f3b5a4d4047
fedorrik <fedorrik1@gmail.com>
2022-08-23 15:04:46 +0300

    update revcomp script

--- scripts/revcomp.py
+++ scripts/revcomp.py
@@ -1,8 +1,13 @@
+from Bio.Seq import Seq
 from Bio.SeqIO import parse
+from os.path import exists
 from sys import argv
 
-
-data = [[record.id, record.seq] for record in parse(argv[1], 'fasta')]
-for i in sorted(data):
-    print('>{}\n{}'.format(i[0], i[1].reverse_complement()))
+if exists(argv[1]):
+    data = [[record.id, record.seq] for record in parse(argv[1], 'fasta')]
+    for i in sorted(data):
+        print('>{}\n{}'.format(i[0], i[1].reverse_complement()))
+else:
+    seq = Seq(argv[1])
+    print(str(seq.reverse_complement()))
 
