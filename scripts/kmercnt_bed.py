65b7533b0addeedfd6869fc5a80af869d4e069dd
fedorrik <fedorrik1@gmail.com>
2022-08-25 12:48:54 +0300

    add bed output to kmercnt script

---
+++ scripts/kmercnt_bed.py
@@ -0,0 +1,13 @@
+from Bio.SeqIO import parse
+from re import finditer
+from sys import argv
+
+
+kmer = argv[2].upper()
+
+total_hits = 0
+for record in parse(argv[1], 'fasta'):
+    matches_coords = [(m.start(0), m.end(0)) for m in finditer(kmer, str(record.seq))]
+    for i in matches_coords:
+        print(record.id, i[0], i[1], sep='\t')
+
