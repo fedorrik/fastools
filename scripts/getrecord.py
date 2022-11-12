6b8fab7cfd28859008da1c712d8460ad67ac2dac
fedorrik <fedorrik1@gmail.com>
2022-11-13 00:09:10 +0300

    getrecord for gz

--- scripts/getrecord.py
+++ scripts/getrecord.py
@@ -1,12 +1,23 @@
 from Bio.SeqIO import parse, write
+from gzip import open
 from sys import argv
 
 
 wanted_id = argv[1]
 infa = argv[2]
 
-for record in parse(infa, 'fasta'):
-    if record.id == wanted_id:
-        print('>{}\n{}'.format(record.id, record.seq))
-        break
+
+if infa[-3:] == '.gz':
+    with open(infa, 'rt') as handle:
+        for record in parse(handle, 'fasta'):
+            if record.id == wanted_id:
+                print('>{}\n{}'.format(record.id, record.seq))
+                break
+
+else:
+    for record in parse(infa, 'fasta'):
+        if record.id == wanted_id:
+            print('>{}\n{}'.format(record.id, record.seq))
+            break
+
 
