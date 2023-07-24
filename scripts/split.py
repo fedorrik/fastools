92c2bc032c94aef0b0741a61d369ca49f703c9d7
fedorrik <fedorrik1@gmail.com>
2023-07-24 22:13:15 +0300

    split works with gzip

--- scripts/split.py
+++ scripts/split.py
@@ -1,8 +1,18 @@
-from sys import argv
+import gzip
 from Bio.SeqIO import parse, write
+from sys import argv
 
 
+infa = argv[1]
 out_dir = argv[2]
-for record in parse(argv[1], 'fasta'):
-    write(record, '{}/{}.fa'.format(out_dir, record.id), 'fasta')
+
+
+if infa[-3:] == '.gz':
+    with gzip.open(infa, 'rt') as handle:
+        for record in parse(handle, 'fasta'):
+            write(record, '{}/{}.fa'.format(out_dir, record.id), 'fasta')
+
+else:
+    for record in parse(infa, 'fasta'):
+        write(record, '{}/{}.fa'.format(out_dir, record.id), 'fasta')
 
