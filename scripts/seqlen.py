7a5241b4501a065ecbd17714db11b38e532d2618
fedor <fedorrik1@gmail.com>
2024-02-06 15:06:34 +0300

    implemented gz support for seqlen

--- scripts/seqlen.py
+++ scripts/seqlen.py
@@ -1,6 +1,16 @@
+import gzip
 from sys import argv
 from Bio.SeqIO import parse
 
-for record in parse(argv[1], 'fasta'):
-    print(record.id, len(record.seq), sep='\t')
+
+infa = argv[1]
+
+if infa[-3:] == '.gz':
+    with gzip.open(infa, 'rt') as handle:
+        for record in parse(handle, 'fasta'):
+            print(record.id, len(record.seq), sep='\t')
+            
+else:
+    for record in parse(infa, 'fasta'):
+        print(record.id, len(record.seq), sep='\t')
 
