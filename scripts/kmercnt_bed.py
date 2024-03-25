d7bf39b506e54eda4732ce34b6b8fbfe9a619b11
fedor <fedorrik1@gmail.com>
2024-03-26 01:11:40 +0300

    add gz support for kmercnt_bed script

--- scripts/kmercnt_bed.py
+++ scripts/kmercnt_bed.py
@@ -17,10 +17,21 @@ infa = argv[1]
 kmer_list = extend_ambiguous_dna(argv[2].upper())
 
 total_hits = 0
-for record in parse(infa, 'fasta'):
-    matches_coords = []
-    for kmer in kmer_list:
-        matches_coords += [(m.start(0), m.end(0), kmer) for m in finditer(kmer, str(record.seq).upper())]
-    for i in matches_coords:
-        print(record.id, i[0], i[1], i[2], sep='\t')
+
+if infa[-3:] == '.gz':
+    with gzip.open(infa, 'rt') as handle:
+        for record in parse(handle, 'fasta'):
+            matches_coords = []
+            for kmer in kmer_list:
+                matches_coords += [(m.start(0), m.end(0), kmer) for m in finditer(kmer, str(record.seq).upper())]
+            for i in matches_coords:
+                print(record.id, i[0], i[1], i[2], sep='\t')        
+
+else:
+    for record in parse(infa, 'fasta'):
+        matches_coords = []
+        for kmer in kmer_list:
+            matches_coords += [(m.start(0), m.end(0), kmer) for m in finditer(kmer, str(record.seq).upper())]
+        for i in matches_coords:
+            print(record.id, i[0], i[1], i[2], sep='\t')
 
