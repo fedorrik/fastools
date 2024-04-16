16aa8d68a5254cb10ea278e59b90611a9dbfad9a
fedor <fedorrik1@gmail.com>
2024-04-16 13:06:11 +0300

    kmercnt removes all gaps from fasta before counting

--- scripts/kmercnt.py
+++ scripts/kmercnt.py
@@ -3,7 +3,7 @@ from Bio import Seq
 from Bio.Data import IUPACData
 from Bio.SeqIO import parse
 from itertools import product
-from re import findall
+from re import findall, sub
 from sys import argv
 
 
@@ -21,17 +21,19 @@ total_hits = 0
 if infa[-3:] == '.gz':
     with gzip.open(infa, 'rt') as handle:
         for record in parse(handle, 'fasta'):
+            seq = sub('-', '', str(record.seq))
             n_hits = 0
             for kmer in kmer_list:
-                n_hits += len(findall(kmer, str(record.seq).upper()))
+                n_hits += len(findall(kmer, seq.upper()))
             print(record.id, n_hits, sep='\t')
             total_hits += n_hits          
 
 else:
     for record in parse(infa, 'fasta'):
+        seq = sub('-', '', str(record.seq))
         n_hits = 0
         for kmer in kmer_list:
-            n_hits += len(findall(kmer, str(record.seq).upper()))
+            n_hits += len(findall(kmer, seq.upper()))
         print(record.id, n_hits, sep='\t')
         total_hits += n_hits
 
--- scripts/kmercnt_bed.py
+++ scripts/kmercnt_bed.py
@@ -3,7 +3,7 @@ from Bio import Seq
 from Bio.Data import IUPACData
 from Bio.SeqIO import parse
 from itertools import product
-from re import finditer
+from re import finditer, sub
 from sys import argv
 
 
@@ -21,17 +21,19 @@ total_hits = 0
 if infa[-3:] == '.gz':
     with gzip.open(infa, 'rt') as handle:
         for record in parse(handle, 'fasta'):
+            seq = sub('-', '', str(record.seq))
             matches_coords = []
             for kmer in kmer_list:
-                matches_coords += [(m.start(0), m.end(0), kmer) for m in finditer(kmer, str(record.seq).upper())]
+                matches_coords += [(m.start(0), m.end(0), kmer) for m in finditer(kmer, seq.upper())]
             for i in matches_coords:
                 print(record.id, i[0], i[1], i[2], sep='\t')        
 
 else:
     for record in parse(infa, 'fasta'):
+        seq = sub('-', '', str(record.seq))
         matches_coords = []
         for kmer in kmer_list:
-            matches_coords += [(m.start(0), m.end(0), kmer) for m in finditer(kmer, str(record.seq).upper())]
+            matches_coords += [(m.start(0), m.end(0), kmer) for m in finditer(kmer, seq.upper())]
         for i in matches_coords:
             print(record.id, i[0], i[1], i[2], sep='\t')
 
